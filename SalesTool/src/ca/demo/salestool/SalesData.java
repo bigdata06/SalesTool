483c90d2ca9a22f24b151354374ae9f709cef92d
bigdata06 <bigdata06@gmail.com>
2014-04-14 23:08:30 -0400

    second change in code

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -1,10 +1,12 @@
 package ca.demo.salestool;
 
+import java.util.Date;
+
 /**
  * Represent the sales data for a given period.
  */
 public class SalesData {
-	int data[] = {-1, 0, 4, 42, 84, 130};
+	int data[] = {-1, 0, 4, 42, 84, 130, 25};
 	
 	public void display() {
 		System.out.println("Data from said: ");
@@ -14,6 +16,9 @@ public class SalesData {
 			System.out.println("Next value: " + data[i]);
 			sum += i;
 		}
+		
+		Date date = new Date();
+		System.out.println("Date is: " + date.toString());
 		
 		System.out.println("Sum is: " + sum);
 	}
